060cbcac2990dcb788f84db25ae5e17f4c1ddbd0
Amarjit Ghosh <amarjitghosh31@gmail.com>
2023-07-02 08:51:12 +0530

    final-commit

--- Team_5/App/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from App.models import CustomMentor
-from App.models import CustomMentee
-# Register your models here.
-admin.site.register(CustomMentor)
-admin.site.register(CustomMentee)
